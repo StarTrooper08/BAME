62580c3c42c9f3130069782152946f7b48e181b7
Atharva Shirdhankar <72031540+StarTrooper08@users.noreply.github.com>
2021-05-23 12:44:20 +0530

    update

--- bame.py
+++ bame.py
@@ -5,11 +5,9 @@ question = input("Enter the question here :")
 answer = input("Enter the Answer here :")
 
 
-'''
-pywk.text_to_handwriting("variable or string here",
+'''  pywk.text_to_handwriting("variable or string here",
       "image_name.png ",   <--   image name including png extension or you can also specify complete path (example : C:\Users\User\Desktop\images\pywkimage.png)
-      rgb=[0,0,255])       <-- color for the font
-'''    
+      rgb=[0,0,255])       <-- color for the font '''    
 
 
 
