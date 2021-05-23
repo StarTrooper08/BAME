e233596d000548990f9a0df1775b25339cf3a0fa
Atharva Shirdhankar <72031540+StarTrooper08@users.noreply.github.com>
2021-05-23 12:48:28 +0530

    update2

--- bame.py
+++ bame.py
@@ -4,11 +4,8 @@ import pywhatkit as pywk
 question = input("Enter the question here :")
 answer = input("Enter the Answer here :")
 
-
-'''  pywk.text_to_handwriting("variable or string here",
-      "image_name.png ",   <--   image name including png extension or you can also specify complete path (example : C:\Users\User\Desktop\images\pywkimage.png)
-      rgb=[0,0,255])       <-- color for the font '''    
+    
 
 
-
+#function
 pywk.text_to_handwriting("Question " + "" + question + "\n" + "Answer " + "" + answer,"imagename.png",rgb=[0,0,255])
